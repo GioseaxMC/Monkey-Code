ee45a7994973df31a3e49c333a833b5add03d2b0
GioseaxMC <gioseaxdiscord@gmail.com>
2024-09-30 21:25:38 +0200

    added some highlightings

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ set "source_dir=.\dist"
 set "destination_zip=.\distribution\Monkey Editor.zip"
 
 rem Compress the directory to a ZIP file
-powershell -command "Compress-Archive -Path '%source_dir%\*' -DestinationPath '%destination_zip%'"
+powershell -command "Compress-Archive -Update -Path '%source_dir%\*' -DestinationPath '%destination_zip%'"
 
 echo Directory compressed to %destination_zip%
 endlocal
